74d9879ef2a13d5d24b3750c176eac1c72ee97a8
Michael ODonnell <modonnell@grassroots-tech.com>
2015-12-31 20:26:19 +0000

    fixing up stecker pair tracker

--- src/main/java/com/demo/crypto/enigma/util/SteckerCombinationTracker.java
+++ src/main/java/com/demo/crypto/enigma/util/SteckerCombinationTracker.java
@@ -21,7 +21,9 @@ public class SteckerCombinationTracker implements Iterator<List<SteckerCable>> {
 	private long combinationsCount = 0;
 	
 	
-	public SteckerCombinationTracker() { }
+	public SteckerCombinationTracker() {
+		this(DEFAULT_STECKER_PAIR_COUNT);
+	}
 	
 	public SteckerCombinationTracker(int steckerPairCount) {
 		if( steckerPairCount < 0 )
@@ -35,14 +37,48 @@ public class SteckerCombinationTracker implements Iterator<List<SteckerCable>> {
 	
 	@Override
 	public boolean hasNext() {
-		if( currentPairs != null && currentPairs.size()==2 ) { // TODO: this should be size 10
-			if( currentPairs.get(0).getInput()=='Y' && currentPairs.get(0).getOutput()=='Z' ) {
-				return( currentPairs.get(1).getInput()=='W' && currentPairs.get(1).getOutput()=='X' );
+		
+		boolean hasNext = true;
+		// every instance of SteckerCombinationTracker returns an empty List first, regardless of steckerPairCount. so, there's a next pair unless:
+		//  * steckerPairCount is 0, AND we've already given out our empty List
+		//      - OR -
+		//  * the rightmost cable is at Y<=>Z, the one to its left is at W<=>X, the one to that cable's left is at U<=>V, etc.
+		
+		if( combinationsCount > 0 ) { // if next() has never been called, hasNext is true.
+			if( steckerPairCount==0 )
+				hasNext = false;
+			
+			else if( currentPairs.isEmpty() ) // if currentPairs is empty, next() has been called exactly once, we have more pairs as long as steckerPairCount>0
+				hasNext = true;
+			
+			else {
+				boolean hasCableThatCanMove = false;
+				
+				// index=0 is the leftmost cable. start at the rightmost and work our way left, seeing if any of our stecker cables can move to the right. if all our
+				// cables are as far right as they can go (for example, if 2 cables are in use and they're at positions W<=>X and Y<=>Z), then we're out of combinations.
+				for( int index=steckerPairCount-1; index>=0; index-- ) {
+					SteckerCable cable = currentPairs.get(index);
+					
+					// if index=2 and steckerPairCount=3, lastInputForThisCable = Y (index 24)
+					// if index=1 and steckerPairCount=3, lastInputForThisCable = W (index 22)
+					// if index=0 and steckerPairCount=3, lastInputForThisCable = U (index 20)
+					char lastInputForThisCable = Alphabet.ALPHABET_ARRAY[25-2*(steckerPairCount-1-index)-1];
+					char lastOutForThisCable = Alphabet.nextCharacter(lastInputForThisCable);
+					
+					if( cable.getInput() != lastInputForThisCable || cable.getOutput() != lastOutForThisCable ) {
+						hasCableThatCanMove = true;
+						break; // this cable is NOT all the way right yet, therefore hasNext() is true!
+					}
+				}
+				
+				if( !hasCableThatCanMove )
+					hasNext = false;
 			}
 		}
 		
-		return true;
+		return hasNext;
 	}
+	
 	
 	@Override
 	public List<SteckerCable> next() {
--- src/test/java/com/demo/crypto/enigma/util/SteckerPairTrackerTest.java
+++ src/test/java/com/demo/crypto/enigma/util/SteckerPairTrackerTest.java
@@ -1,11 +1,11 @@
 package com.demo.crypto.enigma.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import com.demo.crypto.enigma.model.SteckerCable;
@@ -14,46 +14,112 @@ public class SteckerPairTrackerTest {
 	
 	private SteckerCombinationTracker steckerCombinationTracker;
 	
-	@Before
-	public void setup() {
-		steckerCombinationTracker = new SteckerCombinationTracker();
-	}
-	
 	
 	@Test
-	public void testHasNext() {
-		for( int count=0; count<100; count++) {
-			steckerCombinationTracker.next();
-			assertTrue( steckerCombinationTracker.hasNext() );
-		}
+	public void testUnsteckered() {
+		steckerCombinationTracker = new SteckerCombinationTracker(0);
+		
+		// with no stecker cables, the SteckerCombinationTracker should return exactly 1 List (an empty List) of stecker combinations.
+		assertTrue( steckerCombinationTracker.hasNext() );
+		assertTrue( steckerCombinationTracker.next().isEmpty() );
+		assertFalse( steckerCombinationTracker.hasNext() );
+	}
+	
+	@Test
+	public void testOnePair() {
+		steckerCombinationTracker = new SteckerCombinationTracker(1);
 		
 		assertTrue( steckerCombinationTracker.hasNext() );
+		List<SteckerCable> steckerPairs = steckerCombinationTracker.next();
+		assertTrue( steckerPairs.isEmpty() );
+		
+		long possibleCombinationCount = getPossibleCombinationCount(1);
+		for( long count=1; count<=possibleCombinationCount; count++) {
+			assertTrue( steckerCombinationTracker.hasNext() );
+			steckerPairs = steckerCombinationTracker.next();
+			
+			assertEquals( 1, steckerPairs.size() );
+			
+			// while we're here, spot check a few steckers 
+			if(count == 1) { // the first pair (other than the empty set above) should be A<=>B
+				assertEquals( 'A', steckerPairs.get(0).getInput() );
+				assertEquals( 'B', steckerPairs.get(0).getOutput() );
+			}
+			else if(count == 25) { // the 25th pair should be A<=>Z
+				assertEquals( 'A', steckerPairs.get(0).getInput() );
+				assertEquals( 'Z', steckerPairs.get(0).getOutput() );
+			}
+			else if(count == 26) { // after A<=>Z, we're done checking letter A, next pair should be B<=>C
+				assertEquals( 'B', steckerPairs.get(0).getInput() );
+				assertEquals( 'C', steckerPairs.get(0).getOutput() );
+			}
+			else if(count == possibleCombinationCount) { // and the last pair should be Y<=>Z
+				assertEquals( 'Y', steckerPairs.get(0).getInput() );
+				assertEquals( 'Z', steckerPairs.get(0).getOutput() );
+			}
+		}
+		
+		assertFalse( steckerCombinationTracker.hasNext() );
 	}
 	
 	@Test
-	public void testNext() {
-		assertEquals( 0, steckerCombinationTracker.next().size() );
+	public void testNextNPairs() {
+		int numberOfPairs = 2;
 		
-		List<SteckerCable> testSteckerPairs = steckerCombinationTracker.next();
-		/*
-		assertEquals( 3, testSteckerPairs.size() );
-		assertEquals( 'A', testSteckerPairs.get(0).getInput() );
-		assertEquals( 'B', testSteckerPairs.get(0).getOutput() );
-		assertEquals( 'C', testSteckerPairs.get(1).getInput() );
-		assertEquals( 'D', testSteckerPairs.get(1).getOutput() );
+		steckerCombinationTracker = new SteckerCombinationTracker(numberOfPairs);
 		
-		testSteckerPairs = steckerCombinationTracker.next();
-		assertEquals( 3, testSteckerPairs.size() );
-		assertEquals( 'A', testSteckerPairs.get(0).getInput() );
-		assertEquals( 'B', testSteckerPairs.get(0).getOutput() );
-		assertEquals( 'C', testSteckerPairs.get(1).getInput() );
-		assertEquals( 'E', testSteckerPairs.get(1).getOutput() );
+		assertTrue( steckerCombinationTracker.hasNext() );
+		List<SteckerCable> steckerPairs = steckerCombinationTracker.next();
+		assertTrue( steckerPairs.isEmpty() );
 		
-		for(int count=0; count<10000; count++) {
-			System.out.println("~~~~~~~ test stecker pairs: " + testSteckerPairs);
-			testSteckerPairs = steckerCombinationTracker.next();
+		long possibleCombinationCount = getPossibleCombinationCount(numberOfPairs);
+		for( long count=1; count<=possibleCombinationCount; count++) {
+			assertTrue( steckerCombinationTracker.hasNext() );
+			steckerPairs = steckerCombinationTracker.next();
+			
+			assertEquals( numberOfPairs, steckerPairs.size() );
+			
+			// while we're here, spot check a few steckers 
+			if(count == 1) { // the first pairs (other than the empty set above) should be A<=>B and C<=>D
+				assertEquals( 'A', steckerPairs.get(0).getInput() );
+				assertEquals( 'B', steckerPairs.get(0).getOutput() );
+				assertEquals( 'C', steckerPairs.get(1).getInput() );
+				assertEquals( 'D', steckerPairs.get(1).getOutput() );
+			}
+			else if(count == 23) { // the 23rd pairs should be A<=>B and C<=>Z
+				assertEquals( 'A', steckerPairs.get(0).getInput() );
+				assertEquals( 'B', steckerPairs.get(0).getOutput() );
+				assertEquals( 'C', steckerPairs.get(1).getInput() );
+				assertEquals( 'Z', steckerPairs.get(1).getOutput() );
+			}
+			else if(count == possibleCombinationCount) { // and the last pairs should be W<=>X and Y<=>Z
+				assertEquals( 'W', steckerPairs.get(0).getInput() );
+				assertEquals( 'X', steckerPairs.get(0).getOutput() );
+				assertEquals( 'Y', steckerPairs.get(1).getInput() );
+				assertEquals( 'Z', steckerPairs.get(1).getOutput() );
+			}
 		}
-		*/
+		
+		assertFalse( steckerCombinationTracker.hasNext() );
 	}
 	
+	
+	private static long getPossibleCombinationCount( int steckerCableCount ) {
+		long possibleCombinationCount = 0;
+		
+		if(steckerCableCount==1) {
+			/* for n==1, we can calculate the Guass Sum. the stecker cable input will take 25 values (start at A, end at Y). while the input is A there are 25 possible
+			 * outputs, while the input is B there are 24 possible outputs, while the input is Y there is 1 possible output, etc.
+			 * so this looks like:
+			 *   25 + 24 + 23 + 22 ... + 2 + 1
+			 *   which is equal to the Guass summation (25 * 25+1)/2
+			 */
+			possibleCombinationCount = 25*26/2;
+		}
+		else if(steckerCableCount==2)
+			possibleCombinationCount = 50315;
+		// TODO: fill in up through n==10, and maybe come up with a mathematical formula for 1<n<=10
+			
+		return possibleCombinationCount;
+	}
 }
